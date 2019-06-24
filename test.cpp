95f46c12229917d1b6a4d3b7ad27c09e10e4e5cd
satsukisahi <lunareclipse3141@gmail.com>
2019-06-25 00:05:31 +0900

    6/24

--- test.cpp
+++ test.cpp
@@ -2,98 +2,20 @@
 using namespace std;
 typedef long long ll;
 #define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
-#define repf(i, a, b) for(ll i = (a); i < (b) ; i++ )
-const ll mo = 7;
+const ll mo = 1000000007;
 const ll INF = 1LL << 60; //MAX 9223372036854775807
 ll ans = 0;
-template< int mod >
-struct ModInt {
-  int x;
-
-  ModInt() : x(0) {}
-
-  ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
-
-  ModInt &operator+=(const ModInt &p) {
-    if((x += p.x) >= mod) x -= mod;
-    return *this;
-  }
-
-  ModInt &operator-=(const ModInt &p) {
-    if((x += mod - p.x) >= mod) x -= mod;
-    return *this;
-  }
-
-  ModInt &operator*=(const ModInt &p) {
-    x = (int) (1LL * x * p.x % mod);
-    return *this;
-  }
-
-  ModInt &operator/=(const ModInt &p) {
-    *this *= p.inverse();
-    return *this;
-  }
-
-  ModInt operator-() const { return ModInt(-x); }
-
-  ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
-
-  ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
-
-  ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
-
-  ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
-
-  bool operator==(const ModInt &p) const { return x == p.x; }
-
-  bool operator!=(const ModInt &p) const { return x != p.x; }
-
-  ModInt inverse() const {
-    int a = x, b = mod, u = 1, v = 0, t;
-    while(b > 0) {
-      t = a / b;
-      swap(a -= t * b, b);
-      swap(u -= t * v, v);
-    }
-    return ModInt(u);
-  }
-
-  ModInt pow(int64_t n) const {
-    ModInt ret(1), mul(x);
-    while(n > 0) {
-      if(n & 1) ret *= mul;
-      mul *= mul;
-      n >>= 1;
-    }
-    return ret;
-  }
-
-  friend ostream &operator<<(ostream &os, const ModInt &p) {
-    return os << p.x;
-  }
-
-  friend istream &operator>>(istream &is, ModInt &a) {
-    int64_t t;
-    is >> t;
-    a = ModInt< mod >(t);
-    return (is);
-  }
-
-  static int get_mod() { return mod; }
-};
-
 
 int main()
 {
-
-using modint = ModInt< mo >;
-modint n;
+ll n;
 cin >> n;
+ll t[n] = {};
+rep(i, n){
+    cin >> t[i];
+}
 
-cout << n << endl;
-cout << n+23 << endl;
-cout << n*3 << endl;
-cout << n/4 << endl;
-cout << n-10 << endl;
+cout << ans << endl;
+ans;
 return 0;
 }
