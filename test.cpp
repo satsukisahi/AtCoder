5732cc8f19560e46c80af727b91bf2222bee1bcf
satsukisahi <lunareclipse3141@gmail.com>
2019-07-14 07:00:34 +0900

    7/14

--- test.cpp
+++ test.cpp
@@ -3,26 +3,18 @@ using namespace std;
 typedef long long ll;
 #define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
 const ll mo = 1000000007;
-const ll INF = 1LL << 60; //MAX 9223372036854775807
+const ll INF = 1LL << 59; //MAX 9223372036854775807
 ll ans = 0;
 
 int main()
 {
-ll n;
+/* ll n;
 cin >> n;
 ll t[n] = {};
 rep(i, n){
     cin >> t[i];
-}
-deque<ll> q;
-rep(i,n){
-    q.push_back(t[i]);
-}
-rep(i,n){
-    ll p=q.back();
-    q.pop_back();
-    cout << p << endl;
+} */
 
-}
+cout << INF << endl;
 return 0;
 }
