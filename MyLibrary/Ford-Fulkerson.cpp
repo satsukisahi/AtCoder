dbd569b126f05ee59284f5970eb4764f1f54542f
satsukisahi <lunareclipse3141@gmail.com>
2019-08-06 01:32:38 +0900

    8/6

---
+++ MyLibrary/Ford-Fulkerson.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+ll ans = 0;
+
+template <typename flow_t>
+struct FordFulkerson
+{
+  struct edge
+  {
+    ll to;
+    flow_t cap;
+    ll rev;
+    bool isrev;
+  };
+
+  vector<vector<edge>> graph;
+  vector<ll> used;
+  const flow_t INF;
+  ll timestamp;
+
+  FordFulkerson(ll n) : INF(numeric_limits<flow_t>::max()), timestamp(0)
+  {
+    graph.resize(n);
+    used.assign(n, -1);
+  }
+
+  void add_edge(ll from, ll to, flow_t cap)
+  {
+    graph[from].emplace_back((edge){to, cap, (ll)graph[to].size(), false});
+    graph[to].emplace_back((edge){from, 0, (ll)graph[from].size() - 1, true});
+  }
+
+  flow_t dfs(ll idx, const ll t, flow_t flow)
+  {
+    if (idx == t)
+      return flow;
+    used[idx] = timestamp;
+    for (auto &e : graph[idx])
+    {
+      if (e.cap > 0 && used[e.to] != timestamp)
+      {
+        flow_t d = dfs(e.to, t, min(flow, e.cap));
+        if (d > 0)
+        {
+          e.cap -= d;
+          graph[e.to][e.rev].cap += d;
+          return d;
+        }
+      }
+    }
+    return 0;
+  }
+
+  flow_t max_flow(ll s, ll t)
+  {
+    flow_t flow = 0;
+    for (flow_t f; (f = dfs(s, t, INF)) > 0; timestamp++)
+    {
+      flow += f;
+    }
+    return flow;
+  }
+
+  void output()
+  {
+    for (ll i = 0; i < graph.size(); i++)
+    {
+      for (auto &e : graph[i])
+      {
+        if (e.isrev)
+          continue;
+        auto &rev_e = graph[e.to][e.rev];
+        cout << i << "->" << e.to << " (flow: " << rev_e.cap << "/" << e.cap + rev_e.cap << ")" << endl;
+      }
+    }
+  }
+};
+
+int main()
+{
+//宣言 FordFulkerson< ll > g(V);
+//追加 g.add_edge(from,to,cap);
+//結果 ll ans = g.max_flow(to,from);
+ll v , e ;
+cin >> v >> e ;
+FordFulkerson< ll > g(v);
+rep(i,e){
+  ll a , b , c ;
+  cin >> a >> b >> c ;
+  g.add_edge(a,b,c);
+}
+
+cout << g.max_flow(0,v-1) << endl;
+return 0;
+}
---
+++ MyLibrary/warshallfloyd.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+const ll INF = 1LL << 60; //MAX 9223372036854775807
+
+void warshallfloyd(vector<vector<ll>> &d)
+{
+  for (int k = 0; k < d.size(); k++)
+    for (int i = 0; i < d.size(); i++)
+      for (int j = 0; j < d.size(); j++)
+        d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+}
+
+int main()
+{
+ll n;                                        //頂点数
+ll m;                                        //辺数
+vector<vector<ll>> d(n, vector<ll>(n, INF)); //d[u][v]はuからvへコスト　存在しないとINF、d[i][i]=0
+rep(i, n) { d[i][i] = 0; }
+rep(i, m)
+{
+  //input
+}
+warshallfloyd(d);
+//rep(i, n) if (d[i][i] < 0){負閉路が存在}
+
+return 0;
+}
